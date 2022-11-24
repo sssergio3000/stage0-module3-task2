112c07b9a88b094e407b6348e4fc38171312e7aa
Sergey Khaykov <ssserg3000@gmail.com>
2022-11-24 18:24:59 +0200

    commit2

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -3,13 +3,14 @@ package lang.print.gaps.task2;
 public class NamingConvention {
     public static void main(String[] args) {
         int variable = 10;
-        int variableOne = 1;
+        int variable_One = 1;
         int variableTwo = 2;
-        int variableThree = 3;
-        final int VARIABLE_FOUR = 4;
-        final int VARIABLE_FIVE = 5;
+        int variable_Three = 3;
+        final int VARIABLEFOUR = 4;
+        final int VARIABLEFIVE = 5;
         int variableSix = 6;
         int variableSeven = 7;
         int variableEight = 8;
     }
 }
+
